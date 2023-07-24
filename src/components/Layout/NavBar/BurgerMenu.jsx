7aec085f66ee0ff494c56ec5d7464a914d1ab2e0
alech <hamiltonalec@gmail.com>
2023-07-24 11:46:49 +0100

    fix: made menu tabbable

--- src/components/Layout/NavBar/BurgerMenu.jsx
+++ src/components/Layout/NavBar/BurgerMenu.jsx
@@ -1,31 +1,26 @@
 import { useState } from "react";
-import { BurgerMenuContainer, Close, Hamburger } from "./BurgerMenu.styles";
+import {
+  BurgerMenuContainer,
+  Close,
+  Hamburger,
+  MenuButton,
+} from "./BurgerMenu.styles";
 import NavLinks from "./NavLinks/NavLinks";
 
 const BurgerMenu = () => {
   const [open, setOpen] = useState(false);
 
-  const hamburgerIcon = (
-    <Hamburger
-      onClick={() => {
-        setOpen(!open);
-      }}
-    />
-  );
-
-  const closedIcon = (
-    <Close
-      onClick={() => {
-        setOpen(!open);
-      }}
-    />
-  );
-
   return (
-    <BurgerMenuContainer>
-      {open ? closedIcon : hamburgerIcon}
+    <>
+      <MenuButton
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        {open ? <Close /> : <Hamburger />}
+      </MenuButton>
       <NavLinks open={open} setOpen={setOpen} />
-    </BurgerMenuContainer>
+    </>
   );
 };
 
--- src/components/Layout/NavBar/BurgerMenu.styles.js
+++ src/components/Layout/NavBar/BurgerMenu.styles.js
@@ -4,10 +4,7 @@ import { BiMenu, BiX } from "react-icons/bi";
 export const Hamburger = styled(BiMenu).attrs((props) => ({
   size: "2rem",
 }))`
-  position: absolute;
-  right: 2rem;
   cursor: pointer;
-  z-index: 999;
   color: var(--white);
   transition: 0.3s;
   &:hover {
@@ -18,15 +15,23 @@ export const Hamburger = styled(BiMenu).attrs((props) => ({
 export const Close = styled(BiX).attrs((props) => ({
   size: "2rem",
 }))`
-  position: fixed;
-  right: 2rem;
   cursor: pointer;
-  z-index: 999;
   color: var(--white);
   transition: 0.3s;
   &:hover {
     color: var(--pink);
   }
+`;
+
+export const MenuButton = styled.button`
+  margin: 0;
+  padding: 0;
+  background: none;
+  position: fixed;
+  right: 2rem;
+  top: 2rem;
+  z-index: 9999;
+  border: none;
 `;
 
 export const BurgerMenuContainer = styled.div`
