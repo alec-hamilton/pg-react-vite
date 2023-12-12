b5a90e59a55fab782f74342bdc54ae8484641358
alec-hamilton <hamiltonalec@gmail.com>
2023-12-12 14:01:57 +1100

    feat: added supabase, fetches data

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,8 +3,21 @@ import List from "./components/List/List";
 import { SeasonContainer } from "./Home.styles";
 import { HorizontalLine } from "../../components/Dividers/Dividers.styles";
 import mixData from "../../data/mixData";
+import { useEffect, useState } from "react";
+import { supabase } from "../../lib/helpers/supabaseClient";
 
 const Home = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    const { data } = await supabase.from("episodes").select(`id, season, number, artist, image, mix_url`);
+    setData(data);
+    console.log(data);
+  }
+
   return (
     <>
       {mixData.map((season) => {
