f117815c2f5e7652ed7992b5d27d1e8bfff3ae1a
alec-hamilton <hamiltonalec@gmail.com>
2023-12-12 15:51:32 +1100

    feat: supabase integration complete

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,25 +2,61 @@ import Season from "./components/Season/Season";
 import List from "./components/List/List";
 import { SeasonContainer } from "./Home.styles";
 import { HorizontalLine } from "../../components/Dividers/Dividers.styles";
-import mixData from "../../data/mixData";
 import { useEffect, useState } from "react";
 import { supabase } from "../../lib/helpers/supabaseClient";
 
 const Home = () => {
   const [data, setData] = useState([]);
+  
   useEffect(() => {
-    getData();
+    fetchData();
   }, []);
 
-  async function getData() {
-    const { data } = await supabase.from("episodes").select(`id, season, number, artist, image, mix_url`);
-    setData(data);
-    console.log(data);
+  async function fetchData() {
+    let { data: episodes, error } = await supabase.from("episodes").select(`
+            number, artist, image, mix_url,
+            season: seasons (
+                number,
+                visual_artist: visual_artists (
+                    name, website_url
+                )
+            )
+        `);
+
+    if (error) {
+      console.log("Error", error);
+    }
+
+    else {
+      let seasons = {};
+      episodes.forEach((ep) => {
+        let seasonNumber = ep.season.number;
+        if (!seasons[seasonNumber]) {
+          seasons[seasonNumber] = {
+            details: {
+              season: seasonNumber,
+              artist: ep.season.visual_artist.name,
+              site: ep.season.visual_artist.website_url,
+            },
+            episodes: [],
+          };
+        }
+
+        seasons[seasonNumber].episodes.push({
+          episode: ep.number,
+          artist: ep.artist,
+          img: ep.image,
+          link: ep.mix_url,
+        });
+      });
+
+      setData(Object.values(seasons).reverse());
+    }
   }
 
   return (
     <>
-      {mixData.map((season) => {
+      {data.map((season) => {
         return (
           <SeasonContainer key={season.details.season}>
             <Season seasonData={season} />
